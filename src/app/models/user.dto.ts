4331e94b627e19ce5666e5870727f93d7c00700a
= <=>
2024-09-24 20:30:52 +0300

    ++userStatus dropdown added to dashboard page.

--- src/app/models/user.dto.ts
+++ src/app/models/user.dto.ts
@@ -2,5 +2,6 @@ export interface UserDTO {
     fullName: string;  // fullName alanı eklendi
     jobTitle: string;  // jobTitle alanı eklendi
     sub: string;       // sub (email) alanı eklendi
+    userStatus: string;       
   }
   
