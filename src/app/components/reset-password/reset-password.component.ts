fcc97bb55af063356b7a9283cdaac20f0ef58e5c
= <=>
2024-10-08 12:05:18 +0300

    ++added readonly keyword to constructors for reset pw page

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -20,13 +20,13 @@ export class ResetPasswordComponent {
     specialChar: false,
   };
   showSuccessPopup: boolean = false;
-  errorMessage: string = '';  // Hata mesajını göstermek için
+  errorMessage: string = ''; // Hata mesajını göstermek için
 
   constructor(
-    private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private authService: AuthenticationService
+    private readonly fb: FormBuilder,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly authService: AuthenticationService
   ) {
     this.resetPasswordForm = this.fb.group({
       newPassword: ['', [Validators.required]],
@@ -38,52 +38,61 @@ export class ResetPasswordComponent {
     this.email = this.route.snapshot.queryParamMap.get('email') || ''; // Email'i de URL'den alıyoruz
     console.log('Token:', this.token);
     console.log('Email:', this.email);
-
   }
 
   onSubmit() {
-    if (this.resetPasswordForm.valid && this.isPasswordValid() && !this.passwordsDoNotMatch) {
+    if (
+      this.resetPasswordForm.valid &&
+      this.isPasswordValid() &&
+      !this.passwordsDoNotMatch
+    ) {
       const newPassword = this.resetPasswordForm.get('newPassword')?.value;
-  
+
       const passwordResetRequest = {
-        email: this.email,  // URL'den aldığımız email
+        email: this.email, // URL'den aldığımız email
         newPassword: newPassword,
       };
-  
+
       // Şifre sıfırlama isteği
-      this.authService.resetPassword(this.token, passwordResetRequest).subscribe(
-        (response) => {
-          if (response.success) {
-            console.log('Password reset successfully', response);
-            this.showSuccessPopup = true;
-            setTimeout(() => {
-              this.showSuccessPopup = false;
-              this.router.navigate(['/login']);
-            }, 2000); // 2 saniye sonra login sayfasına yönlendir
-          } else {
-            // Başarısız durumda hata mesajını göster
-            this.errorMessage = response.message || 'An error occurred while resetting password.';
-             // Hata mesajının 3 saniye sonra kaybolmasını sağla
-          setTimeout(() => {
-            this.errorMessage = '';
-          }, 3000); 
+      this.authService
+        .resetPassword(this.token, passwordResetRequest)
+        .subscribe(
+          (response) => {
+            if (response.success) {
+              console.log('Password reset successfully', response);
+              this.showSuccessPopup = true;
+              setTimeout(() => {
+                this.showSuccessPopup = false;
+                this.router.navigate(['/login']);
+              }, 2000); // 2 saniye sonra login sayfasına yönlendir
+            } else {
+              // Başarısız durumda hata mesajını göster
+              this.errorMessage =
+                response.message ||
+                'An error occurred while resetting password.';
+              // Hata mesajının 3 saniye sonra kaybolmasını sağla
+              setTimeout(() => {
+                this.errorMessage = '';
+              }, 3000);
+            }
+          },
+          (error) => {
+            // Backend'den gelen hata mesajını işleyin
+            console.error('Password reset failed', error);
+            this.errorMessage =
+              error.error?.message ||
+              'An unexpected error occurred. Please try again.';
           }
-        },
-        (error) => {
-          // Backend'den gelen hata mesajını işleyin
-          console.error('Password reset failed', error);
-          this.errorMessage = error.error?.message || 'An unexpected error occurred. Please try again.';
-        }
-      );
+        );
     } else {
       this.passwordsDoNotMatch = true;
     }
   }
-  
 
   checkPasswordMatch() {
     const newPassword = this.resetPasswordForm.get('newPassword')?.value;
-    const confirmPassword = this.resetPasswordForm.get('confirmPassword')?.value;
+    const confirmPassword =
+      this.resetPasswordForm.get('confirmPassword')?.value;
 
     this.passwordsDoNotMatch = newPassword !== confirmPassword;
   }
@@ -98,7 +107,9 @@ export class ResetPasswordComponent {
     this.passwordValidations.uppercase = /[A-Z]/.test(password);
 
     // En az bir özel karakter kontrolü
-    this.passwordValidations.specialChar = /[!+@#$%^&*(),.?":{}|<>]/.test(password);
+    this.passwordValidations.specialChar = /[!+@#$%^&*(),.?":{}|<>]/.test(
+      password
+    );
 
     // Şifreler eşleşiyor mu kontrol et
     this.checkPasswordMatch();
