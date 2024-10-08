f098f0ffd8768ef888765c5eacecd48be93b20bb
= <=>
2024-10-08 11:26:35 +0300

    ++import correction for sonarcloud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';  // ReactiveFormsModule'u import edin
 import { RegistrationComponent } from './components/registration/registration.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CalendarModule } from 'primeng/calendar';  // PrimeNG Calendar modülünü import edin
-import { ButtonModule } from 'primeng/button';  // PrimeNG Button modülünü import edin
-import { InputTextModule } from 'primeng/inputtext';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DropdownModule } from 'primeng/dropdown';
-import { DialogModule } from 'primeng/dialog';
-import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
-
+// PrimeNG modules
+import { CalendarModule } from 'primeng/calendar';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
   declarations: [
@@ -26,22 +26,22 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
     HomepageComponent,
     LoginComponent,
     DashboardComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,  
-    HttpClientModule,
     BrowserAnimationsModule,
-    CalendarModule, 
-    ButtonModule,  
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CalendarModule,
+    ButtonModule,
     InputTextModule,
     DropdownModule,
-    DialogModule
+    DialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
